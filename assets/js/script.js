35e597975f1a6fc29ff122d87ced2fcbf655bade
Fabio Madeira <fabio_madeira@outlook.com>
2021-11-23 22:07:50 +0000

    add line breaks on js text for game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ function win(userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} wins over ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} wins over ${convertToWord(computerChoice)}${smallCompWord}.<br> You win!`;
     userChoice_div.classList.add('green-glow');
     setTimeout(function() { userChoice_div.classList.remove('green-glow')}, 800);
 }
@@ -41,7 +41,7 @@ function lose(userChoice, computerChoice) {
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You lost!`;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}.<br> You lost!`;
     userChoice_div.classList.add('red-glow');
     setTimeout(function() { userChoice_div.classList.remove('red-glow')}, 800);
 }
@@ -50,7 +50,7 @@ function draw(userChoice, computerChoice) {
     const smallUserWord = "user".fontsize(3).sup();
     const smallCompWord = "comp".fontsize(3).sup();
     const userChoice_div = document.getElementById(userChoice);
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals to ${convertToWord(computerChoice)}${smallCompWord}. It's a draw!`;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals to ${convertToWord(computerChoice)}${smallCompWord}.<br> It's a draw!`;
     userChoice_div.classList.add('gray-glow');
     setTimeout(function() { userChoice_div.classList.remove('gray-glow')}, 800);
 }
